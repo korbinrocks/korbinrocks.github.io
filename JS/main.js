086fbcc2e3065ad392dfb83cc5fc4cf6bd5b01e0
korbinrocks <118622137+korbinrocks@users.noreply.github.com>
2022-11-19 15:58:43 +0000

    Create main.js

---
+++ JS/main.js
@@ -0,0 +1,37 @@
+function copyToClipboard(){
+    navigator.clipboard.writeText("korbin#3621");
+    
+    element = document.getElementById("alert");
+    fadeIn(element);
+
+    setTimeout(() => {
+        fade(element);
+      }, 2500);
+    
+}
+
+function fade(element) {
+    var op = 1;
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function fadeIn(element) {
+    var op = 0.1;
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
